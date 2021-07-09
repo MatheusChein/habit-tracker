627132758691e6d8fa2c998cd80081f580867228
Luiz Frota <luizgustavo.frota@outlook.com>
2021-07-09 16:16:58 -0300

    Teste commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import { Home } from './pages/Home'
+import { HabitPage } from './pages/HabitPage'
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
+
+
 function App() {
+  
   return (
-    <h1>Habit Tracker</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/habitpage" component={HabitPage} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
-
 export default App;
